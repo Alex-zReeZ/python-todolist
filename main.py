d1905a945e75c04814a28c5fe6477a2482b58368
Alex-zReeZ <alexbeck220@gmail.com>
2024-08-08 13:47:28 +0200

    feat: added start message with commands to use todolist

---
+++ main.py
@@ -0,0 +1,27 @@
+
+task = []
+archived_task = []
+
+
+def show_menu():
+    print("1. Add task")
+    print("2. Delete task")
+    print("3. Archive task")
+    print("4. See tasks")
+    print("5. Exit")
+
+
+def add_task():
+    pass
+
+
+def delete_task():
+    pass
+
+
+def archive_task():
+    pass
+
+
+def see_tasks():
+    pass
